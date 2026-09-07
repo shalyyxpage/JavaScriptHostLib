1fdd45c74938a7b230db5e433595375b73664677
shalyyxpage <shalyyyfdo@gmail.com>
2026-09-08 00:01:42 +0530

    Update whatsapp10ads.js

--- whatsapp10ads.js
+++ whatsapp10ads.js
@@ -1,12 +1,12 @@
 const adLinks = [
-        "https://www.profitablecpmrate.com/ad-link-1-here",  // 1 වෙනි වතාවේදී
-        "https://www.profitablecpmrate.com/ad-link-2-here",  // 2 වෙනි වතාවේදී
-        "https://www.profitablecpmrate.com/ad-link-3-here",  // 3 වෙනි වතාවේදී
-        "https://www.profitablecpmrate.com/ad-link-4-here",  // 4 වෙනි වතාවේදී
-        "https://www.profitablecpmrate.com/ad-link-5-here",  // 5 වෙනි වතාවේදී
-        "https://www.profitablecpmrate.com/ad-link-6-here",  // 6 වෙනි වතාවේදී
-        "https://www.profitablecpmrate.com/ad-link-7-here",  // 7 වෙනි වතාවේදී
-        "https://www.profitablecpmrate.com/ad-link-8-here",  // 8 වෙනි වතාවේදී
-        "https://www.profitablecpmrate.com/ad-link-9-here",  // 9 වෙනි වතාවේදී
-        "https://www.profitablecpmrate.com/ad-link-10-here" // 10 වෙනි වතාවේදී
+        "https://omg10.com/4/6825061", // 1 වෙනි වතාවේදී
+        "https://omg10.com/4/9421366", // 2 වෙනි වතාවේදී
+        "https://omg10.com/4/8442621", // 3 වෙනි වතාවේදී
+        "https://omg10.com/4/9447964", // 4 වෙනි වතාවේදී
+        "https://omg10.com/4/5269605"  // 5 වෙනි වතාවේදී
+        "https://omg10.com/4/6825061", // 1 වෙනි වතාවේදී
+        "https://omg10.com/4/9421366", // 2 වෙනි වතාවේදී
+        "https://omg10.com/4/8442621", // 3 වෙනි වතාවේදී
+        "https://omg10.com/4/9447964", // 4 වෙනි වතාවේදී
+        "https://omg10.com/4/5269605"  // 5 වෙනි වතාවේදී
     ];
