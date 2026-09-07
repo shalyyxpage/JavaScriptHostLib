857a0b0dccfbfae975b21add4aa7baa6716f1510
shalyyxpage <shalyyyfdo@gmail.com>
2026-09-08 00:59:18 +0530

    Create new10ads.js

---
+++ new10ads.js
@@ -0,0 +1,12 @@
+const adLinks = [
+    "https://www.profitablecpmrate.com/ad-link-1-here",  // 1 වෙනි වතාවේදී
+    "https://www.profitablecpmrate.com/ad-link-2-here",  // 2 වෙනි වතාවේදී
+    "https://www.profitablecpmrate.com/ad-link-3-here",  // 3 වෙනි වතාවේදී
+    "https://www.profitablecpmrate.com/ad-link-4-here",  // 4 වෙනි වතාවේදී
+    "https://www.profitablecpmrate.com/ad-link-5-here",  // 5 වෙනි වතාවේදී
+    "https://www.profitablecpmrate.com/ad-link-6-here",  // 6 වෙනි වතාවේදී
+    "https://www.profitablecpmrate.com/ad-link-7-here",  // 7 වෙනි වතාවේදී
+    "https://www.profitablecpmrate.com/ad-link-8-here",  // 8 වෙනි වතාවේදී
+    "https://www.profitablecpmrate.com/ad-link-9-here",  // 9 වෙනි වතාවේදී
+    "https://www.profitablecpmrate.com/ad-link-10-here" // 10 වෙනි වතාවේදී
+];
