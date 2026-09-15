65154b1be128bc6556413df76ac93a3464511fe7
shalyyxpage <shalyyyfdo@gmail.com>
2026-09-15 07:55:07 +0530

    Create 3adslinks.js

---
+++ 3adslinks.js
@@ -0,0 +1,5 @@
+const adLinks = [
+        "https://www.profitablecpmrate.com/ad-link-1-here", 
+        "https://www.profitablecpmrate.com/ad-link-2-here", 
+        "https://www.profitablecpmrate.com/ad-link-3-here"  
+    ];
